6110c75b43c74fbebfc295fb784ce2f7e4874c64
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-20 17:57:44 +0200

    Initial commit

---
+++ src/main/java/com/github/jbence1994/genealogy/GenealogyApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.genealogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenealogyApplication.class, args);
+    }
+
+}
