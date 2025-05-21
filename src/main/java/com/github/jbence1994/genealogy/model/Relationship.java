ab457ab708d5cc3ea41bdff4f5462944750a1679
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-21 10:43:09 +0200

    Add initial enum for family relationships

---
+++ src/main/java/com/github/jbence1994/genealogy/model/Relationship.java
@@ -0,0 +1,17 @@
+package com.github.jbence1994.genealogy.model;
+
+public enum Relationship {
+    GREAT_GRANDFATHER,
+    GREAT_GRANDMOTHER,
+    GRANDFATHER,
+    GRANDMOTHER,
+    FATHER,
+    MOTHER,
+    BROTHER,
+    SISTER,
+    STEP_BROTHER,
+    STEP_SISTER,
+    SPOUSE,
+    COUSIN,
+    OTHER
+}
