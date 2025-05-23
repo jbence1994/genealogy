59eda8fa9df76547210489d275975d8bfa336f9a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-23 09:41:40 +0200

    Add generated value annotation UUID for id field

--- src/main/java/com/github/jbence1994/genealogy/model/Person.java
+++ src/main/java/com/github/jbence1994/genealogy/model/Person.java
@@ -2,6 +2,8 @@ package com.github.jbence1994.genealogy.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -25,6 +27,7 @@ import java.util.UUID;
 public class Person extends PhotoEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String firstName;
